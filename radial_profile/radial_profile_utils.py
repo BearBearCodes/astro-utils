0d7db8a5f9e9aaa94a05d9c7133e8eb4a7301f2a
ndemarch <73549112+ndemarch@users.noreply.github.com>
2022-05-30 15:44:14 -0400

    Update radial_profile_utils.py

--- radial_profile/radial_profile_utils.py
+++ radial_profile/radial_profile_utils.py
@@ -1061,8 +1061,11 @@ def fit_rectangles(
     """
 
     def _append_rect(_num):
+        if _num == 0:
+            _width_in = 0
+        else:
+            _width_in = (_num - 0.5) * min_width
         _width_out = (_num + 0.5) * min_width
-        _width_in = (_num - 0.5) * min_width  # negative inner width for 1st rectangle
         widths_out.append(_width_out)
         widths_in.append(_width_in)
         heights_out.append(height)
